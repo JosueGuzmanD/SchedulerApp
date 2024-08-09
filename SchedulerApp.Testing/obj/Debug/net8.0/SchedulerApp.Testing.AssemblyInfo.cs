024545edc96d8e58c655b033abe7ef3fed0c3879
Robinson Josué Guzmán David <josue.guzman@semicrol.com>
2024-08-09 09:20:56 +0200

    Infinite Loop Correction

--- SchedulerApp.Testing/obj/Debug/net8.0/SchedulerApp.Testing.AssemblyInfo.cs
+++ SchedulerApp.Testing/obj/Debug/net8.0/SchedulerApp.Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulerApp.Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee64c9e0e97f4ffab97e28554c981d10dd7e6210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5936ae0da64781634d93db0efd6ba00bea50a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulerApp.Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulerApp.Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
